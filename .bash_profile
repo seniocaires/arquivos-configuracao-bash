57fdb26a2587d40afdda894d00f468ce2809a36f
Senio Caires <seniocaires@gmail.com>
2017-12-20 17:41:12 -0200

    init

---
+++ .bash_profile
@@ -0,0 +1,4 @@
+#!/bin/bash
+/home/convidado/./login.sh
+/home/convidado/./logout.sh
+exit
---
+++ login.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Bem vindo.. utilize com moderacao!"
+read -p "Pressione qualquer coisa para finalizar..." var
+
